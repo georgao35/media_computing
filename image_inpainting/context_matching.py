49ff9faa5657033e51bc0ed218072b42456b4ee2
gjy <gao.jingyue@outlook.com>
2022-12-23 11:00:49 +0800

    inpainting init

--- image_inpainting/context_matching.py
+++ image_inpainting/context_matching.py
@@ -1,14 +1,81 @@
-import jittor
 import cv2
 import numpy as np
+import jittor as jt
 
 
 def ssd(img1, img2):
     return np.sum(np.square(img1[img2 > 0].astype("float") - img2[img2 > 0].astype("float")))
 
 
-def get_best_match(orig, fillup):
-    pass
+def conv2d(x, w):
+    N, H, W, C = x.shape
+    Kh, Kw, _C, Kc = w.shape
+    assert C == _C
+    xx = x.reindex([N, H - Kh + 1, W - Kw + 1, Kh, Kw, C, Kc], [
+        'i0',  # Nid
+        'i1+i3',  # Hid+Khid
+        'i2+i4',  # Wid+KWid
+        'i5',  # Cid|
+    ])
+    ww = w.broadcast_var(xx)
+    yy = xx * ww
+    y = yy.sum([3, 4, 5])  # Kh, Kw, c
+    return y
+
+
+def to_lab(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+
+def get_best_match(orig, fillup, mask):
+    # scale = [0.81, 0.9, 1.]
+    # get best match. Use FFT to quickly get ssd
+    fillup_lab = to_lab(fillup)
+    orig_lab = to_lab(orig)
+    M = (mask > 0)
+    BM = orig_lab * M
+    B2M = np.sum(np.square(orig_lab), axis=2, keepdims=True) * M[:, :, :1]
+    kern = jt.ones_like(M[:, :, :1, np.newaxis])
+    B2M_res = conv2d(jt.float32(B2M[np.newaxis, ...]), kern)
+
+    A2 = np.sum(np.square(fillup_lab), axis=2, keepdims=True)
+    A2M_res = conv2d(jt.float32(A2[np.newaxis, ...]), jt.float32(M[::-1, ::-1, 0:1, np.newaxis]))
+    ABM_res = conv2d(jt.float32(fillup_lab[np.newaxis, ...]), jt.float32(BM[::-1, ::-1, :, np.newaxis]))
+
+    res = A2M_res + B2M_res - 2 * ABM_res
+
+    return jt.max(res)
+
+
+def get_best_match_prim(orig_scene, match):
+    image_to_compare = orig_scene.copy()
+    r, c, _ = match.shape
+    ir, ic, _ = image_to_compare.shape
+    min_ssd = None
+    for x in range(r):
+        for y in range(c):
+            # compare to sample image to start off with...
+            # mse(imageA, imageB, mask=0)
+            # assume x,y is top left corner,
+            imageA = match[x:x + ir, y:y + ic, :]
+
+            if imageA.shape[0] != ir or imageA.shape[1] != ic:
+                continue
+            # add the mask
+            current_ssd = ssd(imageA, image_to_compare)
+            if current_ssd == None:
+                pass
+            elif min_ssd == None:
+                min_ssd = current_ssd
+                best_sample = imageA
+                best_x = x
+                best_y = y
+            elif min_ssd > current_ssd:
+                min_ssd = current_ssd
+                best_sample = imageA
+                best_x = x
+                best_y = y
+    return best_x, best_y, best_sample
 
 
 def graph_cut(img1, img2, mask):
--- image_inpainting/main.py
+++ image_inpainting/main.py
@@ -2,6 +2,9 @@ import copy
 import os
 import cv2
 import numpy as np
+import time
+
+from context_matching import get_best_match, get_best_match_prim
 
 
 def load_imgs(idx, base_dir='data'):
@@ -29,14 +32,28 @@ def preprocess(orig, mask, local_context_size=80):
     mask = mask[x_min:x_max, y_min:y_max]
     mask_dilated = cv2.dilate(255 - mask, np.ones((local_context_size, local_context_size), dtype=np.uint8))
 
-    # cv2.imshow('dilate', mask_dilated)
+    # cv2.imshow('dilate', orig_win * (mask_dilated > 0))
     # cv2.imshow('orig', mask)
     # cv2.waitKey(0)
 
-    return orig_win * mask_dilated, mask, mask_dilated
+    return orig_win, mask, mask_dilated
+
+
+def tik():
+    global time_base
+    time_base = time.time()
+
+
+def tok(name="(undefined)"):
+    print("Time used in {}: {}".format(name, time.time() - time_base))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     orig_src, mask_src, fillups = load_imgs(1)
     orig_scene, mask_scene, mask_dilated = preprocess(orig_src, mask_src)
+    tik()
+    a = get_best_match(orig_scene, fillups[0], mask_dilated)
+    tok("jittor FFT")
+    # a = get_best_match_prim(orig_scene * (mask_dilated > 0), fillups[0])
+    print(a)
